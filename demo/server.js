8bb8a185542bcf66e1efd384b1e8441a61e45ea2
liamgeschwindt1 <157790835+liamgeschwindt1@users.noreply.github.com>
2026-05-22 12:14:28 +0000

    changes

--- demo/server.js
+++ demo/server.js
@@ -16,6 +16,7 @@ const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:3001';
 app.use('/api', createProxyMiddleware({
   target: BACKEND_URL,
   changeOrigin: true,
+  pathRewrite: { '^/': '/api/' },
 }));
 
 // Serve static files
