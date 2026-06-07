77f5f74bbc3df09f008fa7efe884712c6b74cd7a
liamgeschwindt1 <157790835+liamgeschwindt1@users.noreply.github.com>
2026-06-07 22:34:43 +0000

    Results: vertical bar left / stats right two-column layout; HowItWorks moved after calculator

--- website/src/home/HomePage.jsx
+++ website/src/home/HomePage.jsx
@@ -14,8 +14,8 @@ export default function HomePage() {
         <Hero />
       </section>
       <RevenueCalculator />
-      <SolutionSection />
       <HowOutroundWorks />
+      <SolutionSection />
       <OrbSection />
       <FinalCTA />
     </main>
